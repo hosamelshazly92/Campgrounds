4f078407068233ddce7e70e1307f36f645dc1b26
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-05-29 23:58:10 +0200

    set up mongoose connection

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
+const dbPort = 27017;
+const dbName = 'camp';
+mongoose.connect(`mongodb://localhost:${dbPort}/${dbName}`, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log(`==========> DB server is running on port: ${dbPort}`);
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
