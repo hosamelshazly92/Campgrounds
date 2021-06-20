1818b240a044d62880bf4bab1ffcf4f352fa9daa
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-06-20 21:19:07 +0200

    create error class

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
 
 const dbPort = 27017;
 const dbName = 'camp';
-mongoose.connect(`mongodb://localhost:${ dbPort }/${dbName}`, {
+mongoose.connect(`mongodb://localhost:${ dbPort }/${ dbName }`, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
@@ -48,42 +49,46 @@ app.get('/campgrounds/new', (req, res) => {
     console.log(`==========> requested path: ${ req.url }`);
 });
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${ campground._id }`);
 
     console.log(`==========> requested path: ${ req.url }`);
-});
+}));
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 
     console.log(`==========> requested path: ${ req.url }`);
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
 
     console.log(`==========> requested path: ${ req.url }`);
-});
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${ campground._id }`);
 
     console.log(`==========> requested path: ${ req.url }`);
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 
     console.log(`==========> requested path: ${ req.url }`);
+}));
+
+app.use((err, req, res, next) => {
+    res.send('Something went wrong :(');
 });
 
 const port = 3000;
---
+++ utils/Err.js
@@ -0,0 +1,9 @@
+class Err extends Error {
+    constructor(message, statusCode) {
+        super();
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+} 
+
+module.exports = Err;
