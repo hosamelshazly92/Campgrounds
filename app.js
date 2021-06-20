680bbf06851dfd182091a155761bc0996e1694f4
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-06-20 21:35:20 +0200

    define error handlers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const Err = require('./utils/Err');
 
 const dbPort = 27017;
 const dbName = 'camp';
@@ -50,6 +51,8 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if(!req.body.campground) throw new Err('Invalid input', 400);
+    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${ campground._id }`);
@@ -87,8 +90,13 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     console.log(`==========> requested path: ${ req.url }`);
 }));
 
+app.all('*', (req, res, next) => {
+    next(new Err('Page Not Found', 404));
+});
+
 app.use((err, req, res, next) => {
-    res.send('Something went wrong :(');
+    const { message = 'Something went wrong', statusCode = 500 } = err;
+    res.status(statusCode).send(message);
 });
 
 const port = 3000;
