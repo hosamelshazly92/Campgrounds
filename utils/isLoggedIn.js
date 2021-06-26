540416db93218bb8c92e3e8c6c2b4d0ac7e1b796
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-06-26 17:00:53 +0200

    install multer package

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
-        req.flash('error', 'You have to login.');
+        req.flash('error', 'You need to login.');
         return res.redirect('/login');
     }
 
