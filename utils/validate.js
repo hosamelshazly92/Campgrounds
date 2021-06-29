6dc728304c89e71dd69682f985e0a07f74834c90
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-06-30 00:14:48 +0200

    install sanitize-html package

--- utils/validate.js
+++ utils/validate.js
@@ -1,11 +1,38 @@
-const joi = require('joi');
+const baseJoi = require('joi');
+const sanitizeHTML = require('sanitize-html');
+
+const extension =(joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{ #label }} must not include HTML'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHTML(value, {
+                    allowedTags: [],
+                    allowedAttributes: {}
+                });
+
+                if (clean !== value) {
+                    return helpers.error('string.escapeHTML', { value });
+                }
+
+                return clean;
+            }
+        }
+    }
+});
+
+const joi = baseJoi.extend(extension);
 
 module.exports.campgroundSchema = joi.object({
     campground: joi.object({
-        title: joi.string().required(),
+        title: joi.string().required().escapeHTML(),
         price: joi.number().required().min(0),
         // images: joi.string().required(),
-        location: joi.string().required(),
+        location: joi.string().required().escapeHTML(),
         description: joi.string().required()
     }).required(),
     removeImages: joi.array()
@@ -14,6 +41,6 @@ module.exports.campgroundSchema = joi.object({
 module.exports.reviewSchema = joi.object({
     review: joi.object({
         rating: joi.number().required().min(1).max(5),
-        body: joi.string().required()
+        body: joi.string().required().escapeHTML()
     }).required()
 });
