7564d317234e14cf1caab6ab74aca64229c7f057
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-07-01 00:48:51 +0200

    edit PORT env var

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`==========> server is running on port: ${ port }`);
 });
